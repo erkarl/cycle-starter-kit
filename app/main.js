919d937c3b8d46cad545f65f5e78b30678aeb6cf
Karl Ranna <karl@karlranna.com>
2016-02-08 09:11:24 +0200

    Add initial boilerplate code

--- app/main.js
+++ app/main.js
@@ -1,2 +1,17 @@
+import Rx from 'rx';
 import Cycle from '@cycle/core';
-import CycleDOM from '@cycle/dom';
+import {makeDOMDriver, div} from '@cycle/dom';
+
+function main() {
+  return {
+    DOM: Rx.Observable.of(
+      div('Welcome to CycleCS')
+    )
+  };
+}
+
+const drivers = {
+  DOM: makeDOMDriver('#app')
+};
+
+Cycle.run(main, drivers);
